607b4322d6b33370741611088c9f076b05e3adcb
a. darryl <akeeladarryl@gmail.com>
2024-05-29 15:19:59 +0800

    converting repo to submodules

--- src/routes/authentication.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { signIn, signUp } from "../controllers/employee";
-const authenication = Router()
-
-authenication.post('/auth/register', signUp)
-authenication.post('/auth/login', signIn)
-
-export default authenication
